745289db45392f9092272c2e864fcad3c280fe1e
TimSon777 <dusalimov.tima@yandex.ru>
2023-12-14 17:57:36 +0300

    add success message

--- src/ConsoleApp/CommandHandlers/UploadCommandHandler.cs
+++ src/ConsoleApp/CommandHandlers/UploadCommandHandler.cs
@@ -16,9 +16,19 @@ public sealed class UploadCommandHandler(IAmazonS3 amazonS3, Config config) : IC
             .GetFiles("*.*", SearchOption.TopDirectoryOnly)
             .Where(f => f.Extension.Equals(".jpg", StringComparison.OrdinalIgnoreCase)
                 || f.Extension.Equals(".jpeg", StringComparison.OrdinalIgnoreCase))
-            .Select(async image => await ProcessImageAsync(image, command.Album));
+            .Select(async image => await ProcessImageAsync(image, command.Album))
+            .ToArray();
 
         await Task.WhenAll(tasks);
+
+        var message = tasks.Length switch
+        {
+            0 => "Successful, files were not uploaded",
+            1 => "Successful, 1 file uploaded",
+            _ => $"Successful, {tasks.Length} files uploaded"
+        };
+        
+        await Console.Out.WriteLineAsync(message);
     }
 
     private async Task ProcessImageAsync(FileSystemInfo image, string album)
