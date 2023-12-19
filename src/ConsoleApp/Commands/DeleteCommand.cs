73ea0e1d258118d9920841087db91a8959774d0f
TimSon777 <dusalimov.tima@yandex.ru>
2023-12-19 19:14:36 +0300

    feature: delete command

---
+++ src/ConsoleApp/Commands/DeleteCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace ConsoleApp.Commands;
+
+[Verb("delete")]
+public sealed class DeleteCommand : ICommand
+{
+    [Option("album", Required = true)]
+    public string Album { get; set; } = default!;
+    
+    [Option("photo", Required = false, Default = null)]
+    public string? Photo { get; set; }
+}
